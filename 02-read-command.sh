aace702ca708e4626df1eded1b9c49d75c3cad87
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-06-26 23:07:09 +0530

    two inputs

--- 02-read-command.sh
+++ 02-read-command.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-#prints below command first
-echo "Please enter your username::"
-# read is command 
-read USER_NAME
+# prints below command first
+# echo "Please enter your username::"
+# # read is command 
+# read USER_NAME
 
-echo "user name is $USER_NAME"
+# echo "user name is $USER_NAME"
 
-echo "please enter the password::"
+# echo "please enter the password::"
 
-# here -s is hide the password
-read -s PASSWORD
+# # here -s is hide the password
+# read -s PASSWORD
 
-#test perpose to print password
-echo "password is $PASSWORD"
+# #test perpose to print password
+# echo "password is $PASSWORD"
+
+echo "Enter first and Last name:"
+#passing two inputs
+read FIRST LAST
+echo "First name: $FIRST"
+echo "Last name: $LAST"
