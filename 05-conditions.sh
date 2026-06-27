a64f9d2c481beed3360821180b26cb4e09716e0e
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-06-27 10:24:26 +0530

    spacial varas

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -19,6 +19,6 @@ NUMBER=$1
 # -lt ->
 # -eq ->
 # -ne ->
- if [ $NUMBER -gt 20 ]; then
-    echo "Given number :$NUMBER is greter then 25"
- fi   
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number: $NUMBER is greter then 25"
+fi   
