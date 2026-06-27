f0e3f6a73d995774a74bc1f73f7752595666b749
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-06-27 10:17:22 +0530

    spacial varas

---
+++ 05-conditions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## conditions are if, elif, else, fi
+
+# if [ expression ]; then
+#     //code
+# fi
+
+# if [ expression ]; then
+#     //code
+# elif [ expression ]; then
+#      //code here
+# else
+#     //code here    
+# fi
+
+
+Number=25
+# -gt ->
+# -lt ->
+# -eq ->
+# -ne ->
+ if [ $Number -gt 20]; then
+    echo "Given number :$Number is greter then 25"
+ fi   
