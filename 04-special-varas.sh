18477198fc4fd59affe3f44656df9264ff64b520
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-06-27 09:45:47 +0530

    Special varas

---
+++ 04-special-varas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### Special Variables ###
+
+echo "All args passed to the script: $@"
+echo "Number of vars passed to script: $#"
+echo "script name: $0"
+echo "Present dir: $PWD"
+echo "who is running: $USER"
+echo "Home dire of the current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "PID of recently executed background process: $!"
+echo "ALL args passed to script: $*"
