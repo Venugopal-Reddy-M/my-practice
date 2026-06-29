0c42f640a8f957e3160f58a8023970c88f1a355b
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-06-29 21:41:36 +0530

    O/P redirection to file with tee command

--- 09-O/P_redirection.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]; then
-   echo "run the script root level"
-   exit 1
-fi
---
+++ 09-redirectons.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script_logs_practice "
+LOGS_FILE="/var/log/shell-script_logs_practice/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+   echo "run the script root level"
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0]; then
+      echo"$2 ...FAILURE" | tee -a $LOGS_FILE
+      exit 1
+    else 
+      echo "$2.... SUCCESS " |tee -a $LOGS_FILE
+    fi  
+}
+
+dnf insall nginx -y &>> $LOGS_FILE
+VALIDATE $? "insatll nginx"
