7785e5af3fe67fc31fa237e7d3f2e33d49a3e7c2
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-13 18:09:02 +0530

    actions

--- 11-delete_old_logs.sh
+++ 11-delete_old_logs.sh
@@ -23,8 +23,8 @@ FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime -14)
 ## IFS=internal field separater  
 ################################
 
- while IFS= read -r filepath; do
- echo"Deleteing file:$filepath"
- rm -f $filepath
- echo "Delete file: $filepath"
- done  <<< $FILES_TO_DELETE
+#  while IFS= read -r filepath; do
+#  echo"Deleteing file:$filepath"
+#  rm -f $filepath
+#  echo "Delete file: $filepath"
+#  done  <<< $FILES_TO_DELETE
