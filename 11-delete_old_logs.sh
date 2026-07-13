0d406d8297cc2022ec15233b1e9de00f17b00328
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-13 17:49:50 +0530

    actions

--- 11-delete_old_logs.sh
+++ 11-delete_old_logs.sh
@@ -15,3 +15,17 @@ if [ ! -d $LOGS_DIR ]; then
    echo -e "$LOGS_DIR Does not exit"
    exit 1
 fi
+
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime -14)
+ echo"$FILES_TO_DELETE"
+
+###############################
+## IFS=internal field separater  
+################################
+
+ while IFS=read -r filepath;
+ do
+ echo"Deleteing file:$filepath"
+ rm -f $filepath
+ echo "Delete file: $filepath"
+ done  <<< $FILES_TO_DELETE
