3bf00bde84bc3d0a956cde8f80b1e576948a3898
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-13 18:36:04 +0530

    actions

--- 11-delete_old_logs.sh
+++ 11-delete_old_logs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-#create the directory and file variables
+# Directory containing log files
 LOGS_DIR=/home/ec2-user/app_logs
+# Log file for this script (if you want to log script output)
 LOGS_FILE="$LOGS_DIR/$0.log"
-
+# Color codes for output
 R="\e[31m"
 G="\e[1;32m"
 Y="\e[33m"
 N="\e[0m"
 
+# Check whether the log directory exists
 #check the directory is exit or not
 
 if [ ! -d $LOGS_DIR ]; then
@@ -17,6 +19,7 @@ if [ ! -d $LOGS_DIR ]; then
 else echo -e "$G files are exist...$LOGS_DIR $N"
 fi
 
+# Find all .log files less than 14 days
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime -14)
  echo "$FILES_TO_DELETE"
 
