d1410e2bc9a7474263ac3dba0f5356dc87fcef70
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-13 17:38:23 +0530

    actions

---
+++ 11-delete_old_logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#create the directory and file variables
+LOGS_DIR=/home/ec2-user/app_logs
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+R="\e[31m"
+G="\e[1;32m"
+Y="\e[33m"
+N="\e[0m"
+
+#check the directory is exit or not
+
+if [ ! -d $LOGS_DIR ]; then
+   echo -e "$LOGS_DIR Does not exit"
+   exit 1
+fi
